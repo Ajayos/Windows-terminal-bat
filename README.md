2458a1c3f30f3ad59701a46e2cf57a5181d1b7e8
Ajay o s <akhubsmaster@gmail.com>
2022-07-08 06:05:18 +0000

    👨‍💻CODE BY 🕊️★⃝AJAY O S©️🧚‍♂️

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 
 # Bat files for windows terminal
+ 
 
-############################################ 
-```
+        PROJECT : windows_terminal.bat
+        AUTHER  : Ajay o s   
+        LICENSE : Apache-2.0 
 
-         PROJECT : windows terminal
-         AUTHER  : Ajay o s   
-         LICENSE : Apache-2.0 
-```
-
-############################################
+#
+#
 # INSTALL
 
 # step 1
@@ -20,5 +18,5 @@ for /F %a in ('echo prompt $E ^| cmd') do @set "ESC=%a"
 
 # step 2
 ```
-
+curl https://raw.githubusercontent.com/B-SUDO-003/Windows-terminal.bat/main/install.cmd -s -o Windows-terminal.bat && Windows-terminal.bat && del Windows-terminal.bat
 ```
---
+++ install.cmd
@@ -0,0 +1,29 @@
+@echo
+@echo
+@echo %ESC%[32m############################################ %ESC%[0m
+@echo %ESC%[32m#           %ESC%[31mPROJECT : windows_terminal.bat %ESC%[32m# %ESC%[0m
+@echo %ESC%[32m#           %ESC%[31mAUTHER  : Ajay o s             %ESC%[32m# %ESC%[0m
+@echo %ESC%[32m#           %ESC%[31mLICENSE : Apache-2.0           %ESC%[32m# %ESC%[0m
+@echo %ESC%[32m############################################ %ESC%[0m
+@echo
+@echo
+@echo
+@echo %ESC%[32mWelcome to BlackSudo World%ESC%[0m
+@echo %ESC%[32mInstalling BlackSudo .......%ESC%[0m
+@echo
+@echo
+@cd %systemroot%\system32
+@echo %ESC%[32mInstalling clear %ESC%[0m
+@curl https://raw.githubusercontent.com/B-SUDO-003/Windows-terminal.bat/main/cmd/clear.cmd -s -o clear.cmd
+@echo %ESC%[32mInstalling ls %ESC%[0m
+@curl https://raw.githubusercontent.com/B-SUDO-003/Windows-terminal.bat/main/cmd/ls.cmd -s -o ls.cmd
+@echo %ESC%[32mInstalling gedit  %ESC%[0m
+@curl https://raw.githubusercontent.com/B-SUDO-003/Windows-terminal.bat/main/cmd/gedit.cmd -s -o gedit.cmd
+@echo %ESC%[32mInstalling mysql %ESC%[0m
+@curl https://raw.githubusercontent.com/B-SUDO-003/Windows-terminal.bat/main/cmd/mysql.cmd -s -o mysql.cmd
+@echo %ESC%[32mInstalling mysql- %ESC%[0m
+@curl https://raw.githubusercontent.com/B-SUDO-003/Windows-terminal.bat/main/cmd/mysql-.cmd -s -o mysql-.cmd
+@echo %ESC%[32mInstalling rm %ESC%[0m
+@curl https://raw.githubusercontent.com/B-SUDO-003/Windows-terminal.bat/main/cmd/rm.cmd -s -o rm.cmd
+@echo %ESC%[32mInstalling ifconfig %ESC%[0m
+@curl https://raw.githubusercontent.com/B-SUDO-003/Windows-terminal.bat/main/cmd/ifconfig.cmd -s -o ifconfig.cmd
